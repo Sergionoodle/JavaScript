91913f70f40b64fd20aeb5d21e2c1edc60e6fb2f
Sergionoodle <73190422+Sergionoodle@users.noreply.github.com>
2021-11-18 18:52:53 +0100

    Create variables.js

---
+++ JavaScript/variables.js
@@ -0,0 +1,20 @@
+/*VARIABLES*/
+var x = 9; //la variable x declarada con var es igual a 9
+//la variable se define en un ambito local, es decir que aun que haya una variable aislada se ejecutara igual por asi decirlo 
+
+console.log("variable con var");
+console.log(x);// asi lo imprimimos por consola RECUERDA --> tienes que guardar
+
+let y = 20; //asi lo que hacemos es declarar la variable y que sera igual a 20
+//en un ambito concreto 
+
+console.log("");
+console.log("variable con let");
+console.log(y);
+
+//otra forma de definir una variable es con const
+const Z = 3.14
+console.log("");
+console.log("constante");
+console.log(z);//al darle un valor ya no se podria ni incrementar ni nada, las constantes se escriben en mayusculas
+
